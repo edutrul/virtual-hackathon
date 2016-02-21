ede2a3097616146eb28f890eb59fa1960fe32556
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2016-02-20 19:57:13 -0500

    Remove debugging errors

---
+++ get_description_occupation.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Get description of the occupation.
+ * 
+ * @param string $url_description
+ *   Url of description to request about.
+ * 
+ * @return string
+ *   The occupation description.
+ */
+function _get_description_occupation($url_description) {
+  require_once('simple_html_dom.php');
+  // Create DOM from URL or file
+  $html = file_get_html($url_description);
+  $occupation_description = '';
+  // Find all p 
+  foreach($html->find('p') as $element) {
+    if (strlen(trim($element->plaintext)) >= 250) {
+      $occupation_description = trim($element->plaintext);
+    }
+  }
+  return $occupation_description;
+}
+
+$url_description = "http://www.stepfour.com/jobs/v9/911364010.htm";
+print json_encode(array('description' => _get_description_occupation($url_description)));
+exit();
