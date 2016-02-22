a934a5f19e8b39606f069a5ca31a55d03e869f09
ricardocastaneda <castaneda.mr83@gmail.com>
2016-02-21 19:05:16 -0500

    Cambio

--- js/app.js
+++ js/app.js
@@ -149,7 +149,7 @@ app.views.JobDescription = Backbone.View.extend({
 		//this.collection.get(this.url_description);
 		
 		_.each(this.collection.models, function(item) {
-			$('#job-description').html('<h2>' + item.get('description') + '</h2>'+ '<p>' + item.get('description') + '</p>');
+			$('#job-description').html('<h2>' + item.get('occupation') + '</h2>'+ '<p>' + item.get('description') + '</p>');
 			//$('#job-description').html(this.template(item.toJSON()));
 		}, this);
 			
